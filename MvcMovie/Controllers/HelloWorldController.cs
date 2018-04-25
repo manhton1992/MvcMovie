1ce84d5d2d918b586047f777c683eefb2110963b
manh ton Nguyen <manhton1992@gmail.com>
2018-04-25 15:05:37 +0200

    Add project files.

---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;   
+using System.Web.Mvc;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: HelloWorld
+        /*
+         *  use for View Template Hello World Index.cshtml
+         */
+        public ActionResult Index()
+        {
+            return View();
+        } 
+        
+        public ActionResult Welcome(string name, int numTimes =1)
+        {
+            ViewBag.Message = "Hello " + name;
+            ViewBag.NumTimes = numTimes;
+            return View();
+        }
+        /*
+         * this code use only when you try to work with Controller direct 
+         */
+        public string Welcome1(string name, int ID = 1)
+        {
+            
+            return HttpUtility.HtmlEncode("Hello " + name + ", ID : " + ID);
+        }
+
+        public string Index1()   
+        {
+            return "this is my <b>default</b> action...";
+        }
+        
+        
+    }
+}
