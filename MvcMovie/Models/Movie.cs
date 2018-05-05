508be0a3ece45b610bedcfc286ee3f0b18ac7fdc
manh ton Nguyen <manhton1992@gmail.com>
2018-05-05 20:11:34 +0200

    adding search

--- MvcMovie/Models/Movie.cs
+++ MvcMovie/Models/Movie.cs
@@ -13,6 +13,7 @@ namespace MvcMovie.Models
         public string Title { get; set; }
         [Display(Name = "Release Date")]
         [DataType(DataType.Date)]
+        // cover dataAnnotations 
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}" , ApplyFormatInEditMode = true)]
         public DateTime ReleaseDate { get; set; }
         public string Genre { get; set; }
